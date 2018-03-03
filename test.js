96dc6ca5726dbced99a3c7ea342a783a923eb7c4
Susan Moring <susanmoring@gmail.com>
2018-03-03 12:25:40 -0600

    Make our first JavaScript program.

---
+++ test.js
@@ -0,0 +1,14 @@
+var name = "Susan";
+var born = 1991
+var year = 2018
+var age = 26;
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+
+if(age > 40) {
+	console.log("Damn...you are OLD!!");
+} else {
+	console.log("Party on, you party animal");
+}
+
